7f5419e5fcd028b68d6a9278d733f7a651cf997a
Seohyeon Lee <seohyeonlee2020@gmail.com>
2024-11-22 07:00:27 +0000

    feat: script working

--- element.sh
+++ element.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
-read USER_INPUT
-if [[ -z USER_INPUT ]]
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
 then
 echo "Please provide an element as an argument."
 else
-#dheck if user_input matches anything in atomic_number, symbol, or name
-INPUT_SEARCH=$($PSQL "select * from elements where name=(100::varchar) or atomic_number=100 or symbol=(100::varcha
-r)")
-  #if match 
+#dheck if user_input matches anything in atomic_number
+INPUT_SEARCH=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING (atomic_number) WHERE elements.atomic_number=($1::numeric)")
+#if empty
+  if [[ -z $INPUT_SEARCH ]]
+#retry with name
+  then
+  INPUT_SEARCH=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING (atomic_number) WHERE elements.name=('$1'::varchar)")
+  #if also empty
+    if [[ -z $INPUT_SEARCH ]]
+    then
+  #retry with symbol
+    INPUT_SEARCH=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING (atomic_number) WHERE elements.symbol=('$1'::varchar)")
+    fi
+  fi
+
   if [[ ! -z $INPUT_SEARCH ]]
-  #return relevant information: 
-  ATOMIC_MASS = $($PSQL "select * from elements where name=(100::varchar) or atomic_number=100 or symbol=(100::varcha
-r) left join properties using (atomic_number)")
+  then
+  #process input results
+  PROCESSED=$(echo "$INPUT_SEARCH" | sed 's/|/ /g')
+  ARR=($PROCESSED)
+  TYPE=$($PSQL "SELECT type FROM types WHERE type_id=${ARR[6]}::numeric")
+  #echo $PROCESSED type $TYPE
+  #echo test value: $INPUT_SEARCH processed ${ARR[2]}
   #The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
-  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MP_C celsius and a boiling point of $BP_C celsius.
+  echo "The element with atomic number ${ARR[0]} is ${ARR[1]} (${ARR[2]}). It's a $TYPE, with a mass of ${ARR[3]} amu. ${ARR[1]} has a melting point of ${ARR[4]} celsius and a boiling point of ${ARR[5]} celsius."
   else
   #if no matches
   echo "I could not find that element in the database."
+  fi
 fi
 
 
